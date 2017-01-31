505661f2a69984bbe89bf4092295fe635474710b
kevin korngut <kevinkorngut@users.noreply.github.com>
2017-01-31 11:39:16 -0800

    adding autoenv back into the installation list

--- brew.sh
+++ brew.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Check for Homebrew and install it if missing
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
@@ -8,13 +18,18 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
-# Save Homebrew’s installed location.
-BREW_PREFIX=$(brew --prefix)
+# Taps
+brew tap homebrew/apache
+brew tap homebrew/bundle
+brew tap homebrew/core
+brew tap homebrew/dupes
+brew tap homebrew/php
+brew tap homebrew/services
+brew tap homebrew/versions
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
@@ -23,26 +38,25 @@ brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
 # Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
 brew install bash
 brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
 fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
 
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
 brew install homebrew/php/php56 --with-gmp
 
 # Install font tools.
@@ -51,49 +65,36 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
+brew install autoenv
+brew install exiv2
+brew install exiftool
 brew install git
-brew install git-lfs
+brew install git-extras
+brew install heroku
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
+brew install mas
+brew install node
+brew install openssl
 brew install rename
-brew install rlwrap
-brew install ssh-copy-id
 brew install tree
-brew install vbindiff
+brew install yarn
+brew install webkit2png
 brew install zopfli
+
+#Pages
+mas install 409201541
+#Numbers
+mas install 409203825
+#Marked 2
+mas install 890031187
+#DaisyDisk
+mas install 411643860
+#Postico (postgres GUI)
+mas install 1031280567
+#Fantastical 2
+mas install 975937182
 
 # Remove outdated versions from the cellar.
 brew cleanup
