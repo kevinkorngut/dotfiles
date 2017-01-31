af7b55c9a5a71c9f16f21adfa4bb52be79cdabdd
kevin korngut <kevinkorngut@users.noreply.github.com>
2017-01-31 11:32:36 -0800

    updating brew.sh with preferred list of apps

--- brew.sh
+++ brew.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Check for Homebrew and install it if missing
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
@@ -7,6 +17,15 @@ brew update
 
 # Upgrade any already-installed formulae.
 brew upgrade
+
+# Taps
+brew tap homebrew/apache
+brew tap homebrew/bundle
+brew tap homebrew/core
+brew tap homebrew/dupes
+brew tap homebrew/php
+brew tap homebrew/services
+brew tap homebrew/versions
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
@@ -22,7 +41,6 @@ brew install gnu-sed --with-default-names
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
-brew tap homebrew/versions
 brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
@@ -33,12 +51,6 @@ fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
-
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
 
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
@@ -53,53 +65,35 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
 brew install ack
-brew install dark-mode
-#brew install exiv2
+brew install exiv2
+brew install exiftool
 brew install git
-brew install git-lfs
+brew install git-extras
+brew install heroku
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
+brew install mas
+brew install node
+brew install openssl
 brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install testssl
 brew install tree
-brew install vbindiff
+brew install yarn
 brew install webkit2png
 brew install zopfli
+
+#Pages
+mas install 409201541
+#Numbers
+mas install 409203825
+#Marked 2
+mas install 890031187
+#DaisyDisk
+mas install 411643860
+#Postico (postgres GUI)
+mas install 1031280567
+#Fantastical 2
+mas install 975937182
 
 # Remove outdated versions from the cellar.
 brew cleanup
