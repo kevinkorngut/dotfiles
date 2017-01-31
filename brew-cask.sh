0437a211df4f360a87ca12e0fdcda4a5244c4d96
kevin korngut <kevinkorngut@users.noreply.github.com>
2017-01-31 11:32:54 -0800

    adding a separate file for brew cask installations

---
+++ brew-cask.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Check for Homebrew and install it if missing
+if test ! $(which brew)
+then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Taps
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+# Install packages
+apps=(
+    atom
+    alfred
+    dropbox
+    google-chrome
+    google-drive
+    intellij-idea
+    firefox
+    kaleidoscope
+    slack
+)
+
+brew cask install "${apps[@]}"
+
+# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
+
+# Remove outdated versions from the cellar.
+brew cleanup
